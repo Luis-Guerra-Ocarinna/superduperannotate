69e01d7aa27764fbe487070cd976db4109d653f5
Luis Guerra <luis.guerra.rosa@outlook.com>
2025-12-17 22:11:52 -0300

    feat: add existing features
    * function to automate GUI actions
    * shortcuts with custom key bindings
    * proto-assistant: just auto focus/select when editing for now

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,1 @@
-import { HelloWorld } from './hello-world'
-import { renderFreely } from './utils'
-
 export * as utils from './utils'
-export * from './hello-world'
-
-const dispose = renderFreely(HelloWorld)
-setTimeout(dispose, 5 * 1000)
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { render } from 'solid-js/web'
 
 class AssertionError extends Error { }
-export const assert = (condition: any, msg?: string) => { if (!condition) throw new AssertionError(msg) }
+export function assert(condition: unknown, msg?: string): asserts condition { if (!condition) throw new AssertionError(msg) }
 
 export const renderFreely = (code: Parameters<typeof render>[0]) => {
     assert(document.body, 'No body in DOM')
@@ -15,4 +15,33 @@ export const renderFreely = (code: Parameters<typeof render>[0]) => {
         dispose()
         container.remove()
     }
+}
+
+export const sleep = (ms: number) => new Promise(r => setTimeout(r, ms))
+
+type EventOrCustom<T> = T extends Event ? T : CustomEvent<T>
+export class TypedEventTarget<TDetails extends Record<string, Event | any>> extends EventTarget {
+    addEventListener<K extends keyof TDetails & string>(
+        type: K,
+        callback:
+            ((evt: EventOrCustom<TDetails[K]>) => void)
+            |
+            { handleEvent(object: EventOrCustom<TDetails[K]>): void }
+            | null,
+        options?: AddEventListenerOptions | boolean
+    ): void { return super.addEventListener(type, callback as any, options) }
+
+    removeEventListener<K extends keyof TDetails & string>(
+        type: K,
+        callback:
+            ((evt: EventOrCustom<TDetails[K]>) => void)
+            |
+            { handleEvent(object: EventOrCustom<TDetails[K]>): void }
+            | null,
+        options?: AddEventListenerOptions | boolean
+    ): void { return super.removeEventListener(type, callback as any, options) }
+
+    dispatchEvent<K extends keyof TDetails>(
+        event: EventOrCustom<TDetails[K]>
+    ): boolean { return super.dispatchEvent(event) }
 }
