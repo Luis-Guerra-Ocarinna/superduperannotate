db53ee3ad3bc91363c4070368fa00845d19a90a9
Luis Guerra <luis.guerra.rosa@outlook.com>
2026-01-22 20:33:25 -0300

    feat: add timeout fallback for initialization

--- monkey/src/superduperannotate-injection/index.ts
+++ monkey/src/superduperannotate-injection/index.ts
@@ -3,12 +3,30 @@ import './app';
 import { main } from './app';
 import './meta.js?userscript-metadata';
 
+const start = () => {
+  main()
+}
+
+let ran = false
+const timeoutId = setTimeout(() => {
+  if (ran) return
+
+  console.warn('observer timeout reached, running `main` anyway')
+  ran = true
+  start()
+}, 20 * 1_000)
+
 observe(document.body, () => {
+  if (ran) return true
+
   const $triggerAppeared = document.querySelector('app-vector sa-editors-layout app-header-panel sa-editor-workflow>button[data-qa-id="status-button"]')
 
   if (!$triggerAppeared) return
 
+  ran = true
+  clearTimeout(timeoutId)
+
   // delay just a bit to ensure app is prepare to receive commands
-  setTimeout(main, 500)
+  setTimeout(start, 500)
   return true
 })
